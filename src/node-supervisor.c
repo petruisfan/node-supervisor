a52e02e680126f7a43cfc06ae391b10a81004bf4
Sam Pullara <sam@macpro.local>
2010-03-21 16:02:19 -0700

    fix node supervisor so it passes all the arguments

--- src/node-supervisor.c
+++ src/node-supervisor.c
@@ -1,11 +1,16 @@
 #define NODE_SUPERVISOR "/usr/local/lib/node-supervisor/cli-wrapper.js"
 #include <unistd.h>
+#include <stdlib.h>
 int main (int argc, char** argv) {
-  return execl(
+  char** argv2 = malloc(sizeof(char*) * (2 + argc));
+  int i;
+  argv2[0] = "node";
+  argv2[1] = NODE_SUPERVISOR;
+  for (i = 0; i < argc; i++) {
+       argv2[i+2] = argv[i];           
+  }
+  return execv(
     NODE_BIN,
-    "node",
-    NODE_SUPERVISOR,
-    argv[argc - 1],
-    (char *)NULL
+    argv2
   );
 }
