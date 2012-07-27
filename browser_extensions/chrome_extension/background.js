64b48ce56b46d3d1e0610833d85692d746ae1cbb
Gio d'Amelio <giodamelio@gmail.com>
2012-07-26 22:24:33 -0700

    Finished the browser extension and the supporting code

--- browser_extensions/chrome_extension/background.js
+++ browser_extensions/chrome_extension/background.js
@@ -1,6 +1,7 @@
 var socket = io.connect("http://localhost:7272");
 
 watchedTabs = [];
+hasConnection = false;
 
 // Array Remove - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
@@ -11,20 +12,26 @@ Array.prototype.remove = function(from, to) {
 
 // Keep the button up to date -------------------------------------------------
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if (watchedTabs.indexOf(tab.id) >= 0) {
-        chrome.browserAction.setIcon({path: "icon_blue.png"});
-        watchedTabs.remove(watchedTabs.indexOf(tab.id));
+    if (hasConnection) {
+        if (watchedTabs.indexOf(tab.id) >= 0) {
+            chrome.browserAction.setIcon({path: "icon_blue.png"});
+            watchedTabs.remove(watchedTabs.indexOf(tab.id));
+        } else {
+            chrome.browserAction.setIcon({path: "icon_orange.png"});
+            watchedTabs.push(tab.id);
+        }
     } else {
-        chrome.browserAction.setIcon({path: "icon_orange.png"});
-        watchedTabs.push(tab.id);
+        var socket = io.connect("http://localhost:7272");
     }
 });
 
 chrome.tabs.onActivated.addListener(function(info) {
-    if (watchedTabs.indexOf(info.tabId) >= 0) {
-        chrome.browserAction.setIcon({path: "icon_orange.png"});
-    } else {
-        chrome.browserAction.setIcon({path: "icon_blue.png"});
+    if (hasConnection) {
+        if (watchedTabs.indexOf(info.tabId) >= 0) {
+            chrome.browserAction.setIcon({path: "icon_orange.png"});
+        } else {
+            chrome.browserAction.setIcon({path: "icon_blue.png"});
+        }
     }
 });
 // ----------------------------------------------------------------------------
@@ -33,4 +40,14 @@ socket.on('reload', function (data) {
     for (tab in watchedTabs) {
         chrome.tabs.reload(watchedTabs[tab]);
     }
+});
+
+socket.on("connect", function (data) {
+    hasConnection = true;
+    chrome.browserAction.setIcon({path: "icon_blue.png"});
+});
+
+socket.on("disconnect", function (data) {
+    hasConnection = false;
+    chrome.browserAction.setIcon({path: "icon_green.png"});
 });
