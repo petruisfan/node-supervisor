1de7d1da4b48f68517961bf1b5e491c20be86690
Petru Isfan <petru.isfan@gmail.com>
2015-06-24 07:27:59 +0300

    Added test file Removed extra breaks in help message

---
+++ test/sampleApp/server.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    //
+    // CORS
+    //
+    res.setHeader("Access-Control-Allow-Origin", "http://cors-client.com");
+    res.send('Hello World!')
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
