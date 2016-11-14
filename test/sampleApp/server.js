b47cbecc91706081d15d8f500aa7a4c4defd14e0
Petru Isfan <petru.isfan@gmail.com>
2016-11-14 17:54:03 +0200

    resolve #200

--- test/sampleApp/server.js
+++ test/sampleApp/server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
